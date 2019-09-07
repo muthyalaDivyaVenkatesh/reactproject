0911c70e7baf5a88fb66eb344359937ed938377c
mdvenkatesh <mdvenkatesh1421@gmail.com>
2019-09-07 13:31:08 +0530

    before using css ejectors

--- src/App.js
+++ src/App.js
@@ -1,39 +1,123 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+// import Radium, { StyleRoot } from 'radium';
+
 
 class App extends Component {
   state = {
 
     persons: [
-      { name: "mdvenkatesh", age: 21 },
-      { name: "mdv", age: 24 },
-      { name: "neha", age: 23 }
+      { 'id': '11', name: "nikitha", age: 21 },
+      { 'id': '22', name: "janu", age: 24 },
+      { 'id': '33', name: "neha", age: 23 }
     ],
-    otherState = "THis is a other state"
+    otherState: "THis is a other state",
+    showPersons: false
   }
 
-  switchNameHandler = () => {
+  // this is es7 way of writting methods
+  deletePersonHandler = (personIndex) => {
+    // we can copy by these method
+    // const persons  = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  // write a method to set 
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    })
+
+    console.log(personIndex);
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+
+    const persons = [... this.state.persons];
+
+    persons[personIndex] = person;
     // console.log("you have succesfully clicked")
     this.setState({
-      persons: [
-        { name: "mdvenk", age: 21 },
-        { name: "md", age: 24 },
-        { name: "janu", age: 22 }
-      ]
-    })
+      persons: persons
+    });
+  }
+
+  // writting a method 
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'green',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      // ':hover': {
+      //   backgroundColor:'lightgreen',
+      //   color:'black'
+      // }
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            console.log(person)
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+            />
+          })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor : 'violet ',
+      //   color:'black'
+      // }
+    }
+
+
+    // create a class of array  
+    // let classes = ['red', 'bold'].join(' ')
+
+    const classes = [];
+    // first check whether the length is  gerater than 2 
+    if (this.state.persons.length <= 2) {
+      classes.push('red') // classes = [red]
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold') // classes = [red,bold]
+    }
+
     // here return parentesses is used to group everything 
     return (
+      // styleRoot is used when we are using media queries we have to wrap 
+      // your entire app  inside styleoot 
+      // <StyleRoot>
       <div className="App">
         <h1>Hi, i am react App</h1>
-        <p>These is relly working</p>
-        <button onClick={this.switchNameHandler}>Switch name </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are sleeping </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <p className={classes.join(' ')}>These is relly working</p>
+        <button
+          style={style}
+          onClick={this.togglePersonHandler}
+        >Toogle persons </button>
+        {persons}
       </div>
     );
   }
@@ -42,4 +126,7 @@ class App extends Component {
 
 }
 
-export default App;
+export default App
+
+// this Rdium making your component as higher order function 
+// export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import './Person.css';
+import Radium from 'radium';
+
 
 // Note we have to here the React because React is used to convert write the jsx
 const person  = (props) =>{
+   
     return (
-    <div>
-        <p> Hi i am {props.name} my age is {props.age}</p>
+    <div className="Person">
+        <p onClick={props.click}> Hi i am {props.name} my age is {props.age}</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}></input>
     </div>
     
     )}
-// we are exporing the person by default 
+
+
 export default person;
 
+
+
+
+
+
+
+
+
+
+
+
+    // here we are diing the media query
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //         width: '450px'
+    //     }
+    // }
+
+// we are exporing the person by default 
+// export default Radium(person);
+
+
+
+// event.
